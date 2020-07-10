3f627793e349ce2374fe685f6cada2115be98689
ek0mike <michaelkasingye@gmail.com>
2020-07-10 12:15:43 -0700

    first commit

---
+++ card_intro/js/scripts.js
@@ -0,0 +1,12 @@
+function openSlideMenu(){
+    document.getElementById('side-menu').style.width
+    = '250px';
+    document.getElementById('main').style.marginLeft
+    = '250px';
+  }
+  function closeSlideMenu(){
+    document.getElementById('side-menu').style.width
+    = '0px';
+    document.getElementById('main').style.marginLeft
+    = '0px';
+  }
